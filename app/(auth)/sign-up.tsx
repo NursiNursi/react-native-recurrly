5d1fe67ebecbe12c1e26fbd3670934e588dececd
muhamadnursiramdan <muhamadnursiramdan@gmail.com>
2026-09-03 16:38:32 +0700

    first commit

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Link } from "expo-router"
+
+const SignUp = () => {
+    return (
+        <View>
+            <Text>SignUp</Text>
+            <Link href="/(auth)/sign-up">Login to your account</Link>
+        </View>
+    );
+};
+
+export default SignUp;
